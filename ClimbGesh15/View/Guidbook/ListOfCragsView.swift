7d8ddf43867785642836062a2e2f5799f6e32747
ultaris <jt.metis@icloud.com>
2021-10-15 01:34:53 +0700

    Удалены комментарии и выровнен код в Списке со скалами.

--- ClimbGesh15/View/Guidbook/ListOfCragsView.swift
+++ ClimbGesh15/View/Guidbook/ListOfCragsView.swift
@@ -8,34 +8,27 @@
 import SwiftUI
 
 struct ListOfCragsView: View {
-
     let climbAreaName: String
     let crags: [Crag]
     var body: some View {
-            List {
-                ForEach(crags) { crag in
-// хочется убрать совсем подсветку ячейки при тапе
-                   
-                    ZStack {
-                        NavigationLink(destination:
-                                ListOfRoutesView(
-                                    imagesOfCrag: crag.imagesOfCrag,
-                                    routes: crag.routes,
-                                    nameOfCrag: crag.nameCrag)
-                        ) {
-                            EmptyView()
-                        }
-                        CragRowView(crag: crag)
+        List {
+            ForEach(crags) { crag in
+                ZStack {
+                    NavigationLink(destination:
+                                    ListOfRoutesView(
+                                        imagesOfCrag: crag.imagesOfCrag,
+                                        routes: crag.routes,
+                                        nameOfCrag: crag.nameCrag)
+                    ) {
+                        EmptyView()
                     }
-                    
+                    CragRowView(crag: crag)
                 }
-                
             }
-            .navigationTitle(climbAreaName)
-//            .listStyle(.grouped)
-            .listStyle(.plain)
-//            .listStyle(.inset)
-            
+        }
+        .navigationTitle(climbAreaName)
+        .listStyle(.plain)
+        
     }
 }
 
@@ -50,12 +43,6 @@ struct ListOfCragsView_Previews: PreviewProvider {
 struct CragRowView: View {
     let crag: Crag
     var body: some View {
-//        ZStack {
-//            Color.gray
-//                .frame(width: 350, height: 200)
-//                .cornerRadius(15)
-//            Text(crag.nameCrag)
-//        }
         ZStack {
             Image(crag.imageCrag)
                 .resizable()
@@ -69,9 +56,8 @@ struct CragRowView: View {
                         Image(systemName: "triangle")
                             .foregroundColor(Color(.white))
                         Text(crag.nameCrag)
-                            
                             .font(.title2)
-                        .foregroundColor(Color(.white))
+                            .foregroundColor(Color(.white))
                         Spacer()
                     }
                     HStack {
